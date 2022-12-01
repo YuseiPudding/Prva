e07c25d2706b07967e92e3495728b169f727a394
YuseiPudding <102042220+YuseiPudding@users.noreply.github.com>
2022-12-01 09:14:12 +0100

    Update ZbrojZnamenki.py

--- ZbrojZnamenki.py
+++ ZbrojZnamenki.py
@@ -1,9 +1,12 @@
 n=int(input("Unesi troznamenkasti cijeli broj: "))
-a=n%10
-n=n-a
-n=n//10
-b=n%10
-n=n-b
-n=n//10
-c=n%10
-print(a+b+c)
+if n>99 & n<1000:
+  a=n%10
+  n=n-a
+  n=n//10
+  b=n%10
+  n=n-b
+  n=n//10
+  c=n%10
+  print(a+b+c)
+else:
+  print("Nije unešen troznamenkasti broj.")
